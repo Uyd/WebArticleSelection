0b1624cef3a600d7fc81caba9f53f7a2e4630b4f
Uyd <3284491990@qq.com>
2019-04-25 18:05:11 +0800

    Files的增删改

---
+++ ArticleSelection/Controllers/FilesController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using LibraryEntity;
+
+namespace ArticleSelection.Views.Test
+{
+    public class FilesController : Controller
+    {
+        private readonly EntityDbContext _context = new EntityDbContext();
+        public IActionResult Index()
+        {
+            var all = _context.AllFiles.ToList();
+            return View(all);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Delete(Files fl)
+        {
+            var file = new Files
+            {
+                ID = Guid.NewGuid(),
+                Title = fl.Title,
+                Content = fl.Content,
+                DateTime = fl.DateTime != null ? fl.DateTime : DateTime.Now,
+                Type = fl.Type
+            };
+            _context.Add(file);
+            _context.SaveChanges();
+
+            return View("Index");
+        }
+    }
+}
--- LibraryEntity/Files.cs
+++ LibraryEntity/Files.cs
@@ -10,7 +10,9 @@ namespace LibraryEntity
     {
         public Guid ID { get; set; }
         public string Title { get; set; }
-        public string Type { get; set; }
         public string Content { get; set; }
+        public TypeEnum Type { get; set; }
+        public DateTime DateTime { get; set; }
+        
     }
 }
